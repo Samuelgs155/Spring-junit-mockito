cb10578c3bf451ef67b537d17027e503e1168201
Samuel Matias Garcia Saboya <samuel.alten@congreso.int>
2020-02-03 10:36:25 +0100

    Primera prueba unitaria

---
+++ spring-boot-unitarias-1/src/main/java/com/spring/unitarias/SpringBootUnitarias1Application.java
@@ -0,0 +1,13 @@
+package com.spring.unitarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitarias1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnitarias1Application.class, args);
+	}
+
+}
